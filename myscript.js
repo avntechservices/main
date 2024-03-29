3976ddfb2dc3897ab72abffaf1d51d823b03007c
NAVEEN <naveenraju804@gmail.com>
2024-03-29 19:58:11 +0530

    my learning commit

--- myscript.js
+++ myscript.js
@@ -1,10 +1,29 @@
+var homeelement;
+var updateselement;
+var contactuselement;
+var aboutelement;
+
 $(document).ready(function() {
     $(".homepage").show();
     $("#updatespage").hide();
     $(".contactuspage").hide();
     $(".aboutpage").hide();
+    homeelement = document.getElementById("homeanchor");
+  updateselement = document.getElementById("updatesanchor");
+  contactuselement = document.getElementById("contactusanchor");
+  aboutelement = document.getElementById("aboutanchor");
 });
 function homefunction() {
+  debugger;
+  
+  homeelement.classList.remove("btn-primary");
+  homeelement.classList.add("btn-success");
+  updateselement.classList.remove("btn-success");
+  updateselement.classList.add("btn-primary");
+  contactuselement.classList.remove("btn-success");
+    contactuselement.classList.add("btn-primary");
+    aboutelement.classList.remove("btn-success");
+    aboutelement.classList.add("btn-primary");
     $(".homepage").show();
     $("#updatespage").hide();
     $(".contactuspage").hide();
@@ -12,35 +31,52 @@ function homefunction() {
 }
 function updatesfunction(){
     debugger;
+
+    updateselement.classList.remove("btn-primary");
+    updateselement.classList.add("btn-success");
+    homeelement.classList.remove("btn-success");
+    homeelement.classList.add("btn-primary");
+    contactuselement.classList.remove("btn-success");
+    contactuselement.classList.add("btn-primary");
+    aboutelement.classList.remove("btn-success");
+    aboutelement.classList.add("btn-primary");
     $("#updatespage").show();
     $(".homepage").hide();
     $(".contactuspage").hide();
     $(".aboutpage").hide();
 }
 
-function updatesbackfunction(){
-    $("#updatespage").hide();
-    $(".homepage").show();
-}
 
 function contactusfunction(){
+    debugger;
+
+    contactuselement.classList.remove("btn-primary");
+    contactuselement.classList.add("btn-success");
+    homeelement.classList.remove("btn-success");
+    homeelement.classList.add("btn-primary");
+    updateselement.classList.remove("btn-success");
+    updateselement.classList.add("btn-primary");
+    aboutelement.classList.remove("btn-success");
+    aboutelement.classList.add("btn-primary");
     $("#updatespage").hide();
     $(".homepage").hide();
     $(".contactuspage").show();
     $(".aboutpage").hide();
 }
-function contactusbackfunction(){
-    $(".contactuspage").hide();
-    $(".homepage").show();
-}
+
 function aboutfunction(){
+    debugger;
+
+    aboutelement.classList.remove("btn-primary");
+    aboutelement.classList.add("btn-success");
+    homeelement.classList.remove("btn-success");
+    homeelement.classList.add("btn-primary");
+    updateselement.classList.remove("btn-success");
+    updateselement.classList.add("btn-primary");
+    contactuselement.classList.remove("btn-success");
+    contactuselement.classList.add("btn-primary");
     $("#updatespage").hide();
     $(".homepage").hide();
     $(".contactuspage").hide();
     $(".aboutpage").show();
-}
-
-function aboutbackfunction() {
-    $(".aboutpage").hide();
-    $(".homepage").show();
 }
